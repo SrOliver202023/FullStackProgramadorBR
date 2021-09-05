f8ab0a2f2ca7691bd0ce9e5841f491907a53be9f
Sroliver202023 <sroliver202023@gmail.com>
2021-09-04 23:02:27 -0300

    007.Javascript-Intermediario/005.Modificando-elementos/

---
+++ 007.Javascript-Intermediario/005.Modificando-elementos/index.js
@@ -0,0 +1,10 @@
+
+document.getElementById("p1")
+    .innerHTML = `<div>nada1</div>`;
+document.getElementById('p2')
+    .innerText = `nada2`;
+document.getElementById('p3')
+    .innerContent =`nada3`
+document.getElementById('p3')
+    .setAttribute("meuatt","ad");
+
