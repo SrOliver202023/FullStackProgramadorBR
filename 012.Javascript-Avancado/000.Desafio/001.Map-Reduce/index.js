be7f4b797abfc2850fb8dbfd8e6d19e6e4938070
Sroliver202023 <sroliver202023@gmail.com>
2021-09-16 21:43:17 -0300

    012.Javascript-Avancado/000.Desafio/

---
+++ 012.Javascript-Avancado/000.Desafio/001.Map-Reduce/index.js
@@ -0,0 +1,38 @@
+const funcionarios =[
+{
+    name:"Carlos",
+    age: 15,
+    money: 600,
+    job: "Motorista"
+},
+{
+    name:"Lucia",
+    age: 23,
+    money: 800,
+    job: "Adm"
+},
+{
+    name:"Mateus",
+    age: 27,
+    money: 1200,
+    job: "Logistica"
+},
+{
+    name:"Bruna",
+    age: 19,
+    money: 900,
+    job: "Adm"
+},
+{
+    name:"Joao",
+    age: 45,
+    money: 1500,
+    job: "Deposito"
+},
+{
+    name:"Henrrique",
+    age: 24,
+    money: 1500,
+    job: "Deposito"
+}
+];
