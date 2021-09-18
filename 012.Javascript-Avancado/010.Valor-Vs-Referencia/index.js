8b42c2ff920f40950c9db0161307b44281a45902
Sroliver202023 <sroliver202023@gmail.com>
2021-09-17 21:52:32 -0300

    012.Javascript-Avancado/010.Valor-Vs-Referencia/

---
+++ 012.Javascript-Avancado/010.Valor-Vs-Referencia/index.js
@@ -0,0 +1,25 @@
+function novoAluno(nome, idade){
+    return {nome, idade}
+}
+let alunos = [
+    novoAluno("Mario", 23),
+    novoAluno("Maria", 18),
+    novoAluno("Lucas", 33),
+    novoAluno("Julia", 15),
+    novoAluno("Matheus", 43)
+]
+function alunoDaquiA5Anos(aluno){
+    // aluno.idade += 5; está modificando os valores originais do objeto!
+    return { name: aluno.nome }
+}
+function filtreIdade(aluno){
+    if(aluno.idade <= 25){
+        return aluno;
+    }
+}
+// let procureAlunos = alunos.filter(filtreIdade);
+// console.log(procureAlunos);
+// let tt = procureAlunos.map(alunoDaquiA5Anos);
+// console.log(tt);
+let toSearch = alunos.filter(filtreIdade).map(alunoDaquiA5Anos);
+console.log(toSearch);
