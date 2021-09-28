a3f48b638d11b8ccc44af37fb82998d9498959f4
sroliver202023 <sroliver202023@gmail.com>
2021-09-28 11:09:49 -0300

    start front-end

--- 027.Mongoose/src/controllers/LinkController.js
+++ 027.Mongoose/src/controllers/LinkController.js
@@ -1,15 +1,37 @@
 const Link = require('../models/Link');
-module.exports = {
-  async title(req, res){
-    let title = req.params.title;
-    try{
-      let docs = await Link.findOne({ title })
-      res.send(docs);
-    }catch(error){
-      res.send(error);
-    }
-  }
-  
 
-
+const redirect = async (req, res)=>{
+  let title = req.params.title;
+  try{
+    let docs = await Link.findOne({ title })
+    res.redirect(docs.url);
+  }catch(error){ res.send(error) }
 }
+
+const addLink = async(req, res)=>{
+  let link = new Link(req.body);
+  try{
+    let doc = await link.save();
+    console.log(req.body)
+  }catch(error){
+    res.status(422).render('index', { error, body:req.body })
+  }
+}
+
+const indexRegister = async(req, res)=>{
+  res.render('index', { error:false, body:{} });
+}
+
+const allLinks = async(req, res)=>{
+  try{
+    let links = await Link.find({});
+
+    res.render('all', { links })
+    console.log(links.map(link =>link.title))
+
+  }catch(error){
+    res.status(404).render('index', { error, body:req.body })
+  }
+}
+
+module.exports = { redirect, addLink, indexRegister, allLinks};
--- 027.Mongoose/src/db.js
+++ 027.Mongoose/src/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://localhost/newlinks`)
+mongoose.connect(`mongodb://localhost/newlinks`);
 //   .then(()=> console.log(`db It's OK!`))
 //   .catch(error=>console.log(`Error Detected`)
 // );
-
 module.exports = mongoose.connection;
